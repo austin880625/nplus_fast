14506e7e6e79e33d1b8dd0ca2b65d918dc2ca7c2
Darklanx <csdarklanx@gmail.com>
2018-06-28 15:38:09 +0800

    ride_controller index response

--- app/controllers/ride_controller.rb
+++ app/controllers/ride_controller.rb
@@ -1,2 +1,17 @@
 class RideController < ApplicationController
+  def index
+    rides = Ride.all
+    response = Hash.new
+    response[:status] = "succeed"
+    response[:data] = []
+    rides.each do |ride|
+      response[:data].push(ride)
+    end
+    render :json => response
+
+  end
+  
+  def create
+  
+  end
 end
